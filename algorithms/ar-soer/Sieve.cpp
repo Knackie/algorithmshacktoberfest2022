5945f544ad0980b23f6a3937d0b5898efb00780e
sumit0104 <sumit0104gupta@gmail.com>
2020-10-21 10:20:31 +0530

    Sieve C++

---
+++ algorithms/ar-soer/Sieve.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+// Sieve of Eratosthenes is a method to find whether a number is prime or not in O(1) time
+bool is_prime[1000001];
+void sieve()
+{
+    int MaxN = 1000000;
+    for (int i = 0; i <= MaxN; i++)
+        is_prime[i] = 1;
+    is_prime[0] = is_prime[1] = 0; //number 0 and 1 is not prime we know that
+    for (int i = 2; i * i <= MaxN; i++)
+    {
+        if (is_prime[i])
+        {
+            for (int j = i * i; j <= MaxN; j += i)
+            {
+                is_prime[j] = 0;
+            }
+        }
+    }
+
+}
+int main()
+{
+
+    sieve();//preprocessing
+    //enter range
+    int start, end; cin >> start >> end;
+    for (int i = start; i <= end; i++)
+    {
+        if (is_prime[i])
+            cout << i << " ";
+    }
+    cout << "\n";
+
+    return 0;
+}
+
+input: 1 10
+output: 2 3 5 7
