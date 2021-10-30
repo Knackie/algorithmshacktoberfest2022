cb28df4e2932c5873e561a6161330d222d15847e
José Pedro Duarte <jose@switchpayments.com>
2021-10-30 11:22:34 +0100

    Add base strucuture for Spiral Matrix in python

---
+++ algorithms/ar-spmat/SpiralMatrix.py
@@ -0,0 +1,23 @@
+
+
+
+def spiral_matrix(matrix):
+    """
+    Spiral Matrix implementations.
+
+
+    The spiral matrix algorithm takes a 2-Dimensional array of N-rows and M-columns as an input, and orders the
+    elements of this matrix in spiral order.
+
+    Example:
+    [1 , 2, 3]
+    [4 , 5, 6]
+    [7 , 8, 9]
+
+    is printed out in the following order: 1, 2, 3, 6, 9, 8, 7, 4, 5
+
+    :list matrix: 2-Dimensional Array
+    :return: List ordered in spiral order
+    :rtype: list
+    """
+    pass
