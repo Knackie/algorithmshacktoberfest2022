fa5e6667482c7456d667977ccd5e4d3e8fb896dd
Knackie <43431712+Knackie@users.noreply.github.com>
2021-11-02 15:41:19 +0100

    Create configure.sh

---
+++ .github/workflows/configure.sh
@@ -0,0 +1,4 @@
+CXX=g++          # The C++ compiler
+CXXFLAGS=-g       # C++ complilation flags
+NATIVE=on        # compiles code targeted to current hardware
+TUNE=generic     # performance-tuning switch
